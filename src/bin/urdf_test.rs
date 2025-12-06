5b2105087291c5dcec89119052cd0e35cb02b52c
Adam Conkey <adam.conkey@gmail.com>
2025-12-06 15:37:47 -0500

    Get vis in rerun working

--- src/bin/urdf_test.rs
+++ src/bin/urdf_test.rs
@@ -1,4 +1,5 @@
 use nalgebra as na;
+use rerun::external::re_data_loader::UrdfTree;
 use rerun::external::urdf_rs;
 use std::path::PathBuf;
 
@@ -25,45 +26,50 @@ fn main() -> anyhow::Result<()> {
     urdf_path.push("kuka_iiwa");
     urdf_path.push("model.urdf");
 
-    let urdf_robot = urdf_rs::read_file(urdf_path).unwrap();
-    // let links = urdf_robot.links;
-    let joints = urdf_robot.joints;
-    for joint in joints.iter() {
+    let urdf_robot = urdf_rs::read_file(urdf_path.clone()).unwrap();
+    let mut pose = na::Isometry::identity();
+    for joint in urdf_robot.joints.iter() {
         let node = Node::from_pos_rpy(*joint.origin.xyz, *joint.origin.rpy);
-        println!("{:?}", node);
+        pose *= node.pose;
     }
 
-    // let rec = rerun::RecordingStreamBuilder::new("urdf_test").connect_grpc()?;
-    // rec.log_file_from_path(urdf_path, None, true)?;
-    //
-    // rec.set_time_sequence("step", 1);
-    //
-    // let angle = 0.0;
-    // for (joint_index, joint) in urdf.joints().enumerate() {
-    //     if joint.joint_type == urdf_rs::JointType::Revolute {
-    //         let fixed_axis = joint.axis.xyz.0;
-    //
-    //         // // Usually this angle would come from a measurement - here we just fake something:
-    //         // let dynamic_angle = emath::remap(
-    //         //     (step as f64 * (0.02 + joint_index as f64 / 100.0)).sin(),
-    //         //     -1.0..=1.0,
-    //         //     joint.limit.lower..=joint.limit.upper,
-    //         // );
-    //
-    //         // NOTE: each joint already has a fixed origin pose (logged with the URDF file),
-    //         // and Rerun won't allow us to override or add to that transform here.
-    //         // So instead we apply the dynamic rotation to the child link of the joint:
-    //         let child_link = urdf.get_joint_child(joint);
-    //         let link_path = urdf.get_link_path(child_link);
-    //         rec.log(
-    //             link_path,
-    //             &rerun::Transform3D::from_rotation(rerun::RotationAxisAngle::new(
-    //                 fixed_axis,
-    //                 angle as f32,
-    //             )),
-    //         )?;
-    //     }
-    // }
-    //
+    let urdf = UrdfTree::from_file_path(urdf_path.clone())?;
+    let rec = rerun::RecordingStreamBuilder::new("urdf_test").connect_grpc()?;
+    rec.log_file_from_path(urdf_path, None, true)?;
+
+    rec.set_time_sequence("step", 1);
+
+    rec.log(
+        "pose",
+        &rerun::Transform3D::from_translation_rotation(
+            [pose.translation.x, pose.translation.y, pose.translation.z],
+            rerun::Quaternion::from_xyzw([
+                pose.rotation.i as f32,
+                pose.rotation.j as f32,
+                pose.rotation.k as f32,
+                pose.rotation.w as f32,
+            ]),
+        )
+        .with_axis_length(0.2),
+    )?;
+
+    let angle = 0.0;
+    for joint in urdf.joints().into_iter() {
+        if joint.joint_type == urdf_rs::JointType::Revolute {
+            // NOTE: each joint already has a fixed origin pose (logged with the URDF file),
+            // and Rerun won't allow us to override or add to that transform here.
+            // So instead we apply the dynamic rotation to the child link of the joint:
+            let child_link = urdf.get_joint_child(joint);
+            let link_path = urdf.get_link_path(child_link);
+            rec.log(
+                link_path,
+                &rerun::Transform3D::from_rotation(rerun::RotationAxisAngle::new(
+                    joint.axis.xyz.0,
+                    angle as f32,
+                )),
+            )?;
+        }
+    }
+
     Ok(())
 }
